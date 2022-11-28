2713a86a50189a711d1a21384a2d4afe6c6ffb6b
SchiauAlessio <alessio.schiau1@gmail.com>
2022-11-28 19:50:04 +0200

    add lab5

---
+++ labs/lab5-parsing/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Grammar g = new Grammar("src\\resources\\g2.in");
+        System.out.println("Nonterminals: " + g.nonTerminals);
+        System.out.println("Terminals: " + g.terminals);
+        System.out.println("Starting symbol: " + g.startingSymbol);
+        System.out.println("Production set:\n" + g.productionSet);
+        System.out.println(g.checkCFG() ? "CFG" : "Not CFG");
+        while (true) {
+            System.out.print("Enter a nonterminal (or exit to quit): ");
+            String nonterminal = new Scanner(System.in).nextLine().trim();
+            if (nonterminal.equals("exit"))
+                break;
+            List<List<String>> productions = g.productionSet.getProductionsOf(nonterminal);
+            System.out.println(productions.isEmpty() ? "No productions" : productions);
+        }
+    }
+}
